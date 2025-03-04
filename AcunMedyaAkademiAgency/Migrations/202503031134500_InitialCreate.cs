920d2041615ca4824404967518804fd8bee415cc
FATIH\FATIH <fatihxserbest@gmail.com>
2025-03-04 14:50:10 +0300

    Add project files.

---
+++ AcunMedyaAkademiAgency/Migrations/202503031134500_InitialCreate.cs
@@ -0,0 +1,173 @@
+﻿namespace AcunMedyaAkademiAgency.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Abouts",
+                c => new
+                    {
+                        AboutId = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Description = c.String(),
+                        ImageUrl = c.String(),
+                        aboutDate = c.String(),
+                    })
+                .PrimaryKey(t => t.AboutId);
+            
+            CreateTable(
+                "dbo.Admins",
+                c => new
+                    {
+                        AdminId = c.Int(nullable: false, identity: true),
+                        UserName = c.String(),
+                        Password = c.String(),
+                        Name = c.String(),
+                        Surname = c.String(),
+                    })
+                .PrimaryKey(t => t.AdminId);
+            
+            CreateTable(
+                "dbo.Branches",
+                c => new
+                    {
+                        BranchId = c.Int(nullable: false, identity: true),
+                        BranchName = c.String(),
+                    })
+                .PrimaryKey(t => t.BranchId);
+            
+            CreateTable(
+                "dbo.Teams",
+                c => new
+                    {
+                        TeamId = c.Int(nullable: false, identity: true),
+                        NameSurname = c.String(),
+                        ImageUrl = c.String(),
+                        Gender = c.String(),
+                        City = c.String(),
+                        BranchId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.TeamId)
+                .ForeignKey("dbo.Branches", t => t.BranchId, cascadeDelete: true)
+                .Index(t => t.BranchId);
+            
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        CategoryId = c.Int(nullable: false, identity: true),
+                        CategoryName = c.String(),
+                    })
+                .PrimaryKey(t => t.CategoryId);
+            
+            CreateTable(
+                "dbo.Projects",
+                c => new
+                    {
+                        ProjectId = c.Int(nullable: false, identity: true),
+                        ImageUrl = c.String(),
+                        Title = c.String(),
+                        CategoryId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ProjectId)
+                .ForeignKey("dbo.Categories", t => t.CategoryId, cascadeDelete: true)
+                .Index(t => t.CategoryId);
+            
+            CreateTable(
+                "dbo.ProjectDetails",
+                c => new
+                    {
+                        ProjectDetailsId = c.Int(nullable: false, identity: true),
+                        Description = c.String(),
+                        ProjectId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ProjectDetailsId)
+                .ForeignKey("dbo.Projects", t => t.ProjectId, cascadeDelete: true)
+                .Index(t => t.ProjectId);
+            
+            CreateTable(
+                "dbo.Features",
+                c => new
+                    {
+                        FeatureId = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Description = c.String(),
+                        ImageUrl = c.String(),
+                    })
+                .PrimaryKey(t => t.FeatureId);
+            
+            CreateTable(
+                "dbo.Messages",
+                c => new
+                    {
+                        MessageId = c.Int(nullable: false, identity: true),
+                        NameSurname = c.String(),
+                        Email = c.String(),
+                        Title = c.String(),
+                        Description = c.String(),
+                        IsRead = c.Boolean(nullable: false),
+                        SendeDate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.MessageId);
+            
+            CreateTable(
+                "dbo.Notifications",
+                c => new
+                    {
+                        NotificationId = c.Int(nullable: false, identity: true),
+                        NameSurname = c.String(),
+                        Description = c.String(),
+                        ImageUrl = c.String(),
+                    })
+                .PrimaryKey(t => t.NotificationId);
+            
+            CreateTable(
+                "dbo.Services",
+                c => new
+                    {
+                        ServiceId = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Description = c.String(),
+                        ImageUrl = c.String(),
+                    })
+                .PrimaryKey(t => t.ServiceId);
+            
+            CreateTable(
+                "dbo.SocialMedias",
+                c => new
+                    {
+                        SocialMediaId = c.Int(nullable: false, identity: true),
+                        Url = c.String(),
+                        Icon = c.String(),
+                    })
+                .PrimaryKey(t => t.SocialMediaId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.ProjectDetails", "ProjectId", "dbo.Projects");
+            DropForeignKey("dbo.Projects", "CategoryId", "dbo.Categories");
+            DropForeignKey("dbo.Teams", "BranchId", "dbo.Branches");
+            DropIndex("dbo.ProjectDetails", new[] { "ProjectId" });
+            DropIndex("dbo.Projects", new[] { "CategoryId" });
+            DropIndex("dbo.Teams", new[] { "BranchId" });
+            DropTable("dbo.SocialMedias");
+            DropTable("dbo.Services");
+            DropTable("dbo.Notifications");
+            DropTable("dbo.Messages");
+            DropTable("dbo.Features");
+            DropTable("dbo.ProjectDetails");
+            DropTable("dbo.Projects");
+            DropTable("dbo.Categories");
+            DropTable("dbo.Teams");
+            DropTable("dbo.Branches");
+            DropTable("dbo.Admins");
+            DropTable("dbo.Abouts");
+        }
+    }
+}
